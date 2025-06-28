258eb4cc55b9104afc6147e569c6fdc25a2eb5c5
andirese <andirenaldisetiawan@gmail.com>
2025-06-28 22:10:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Undangan Pernikahan Online Aktif!");
+var countDownDate = new Date("Aug 27, 2025 10:00:00").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML =
+    days + " Hari " + hours + " Jam " + minutes + " Menit " + seconds + " Detik";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Acara Sedang Berlangsung";
+  }
+}, 1000);
