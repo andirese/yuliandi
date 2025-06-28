2b879b4d234ceb0a23241720fa9290a227064ca8
andirese <andirenaldisetiawan@gmail.com>
2025-06-28 22:32:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('openInvitation').addEventListener('click', function() {
+  document.querySelector('.landing').style.display = 'none';
+  document.getElementById('content').classList.add('active');
+});
