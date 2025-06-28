bb7ed8d594b73ed7effeb7f169b992ce9fd0fd23
andirese <andirenaldisetiawan@gmail.com>
2025-06-28 23:26:42 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('openInvitation').addEventListener('click', function() {
-  document.querySelector('.landing').style.display = 'none';
-  document.getElementById('content').classList.add('active');
-});
