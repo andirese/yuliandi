ca8a5629f5b0379abd8b07bab4ace9f283f9af93
andirese <andirenaldisetiawan@gmail.com>
2025-06-29 00:01:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-
 function bukaUndangan() {
     document.querySelector('.opening').style.display = 'none';
     document.querySelector('#content').classList.remove('hidden');
+    document.getElementById('bgm').play();
+}
+
+function toggleMusic() {
+    var music = document.getElementById('bgm');
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
 }
